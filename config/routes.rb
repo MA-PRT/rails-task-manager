4f345f5fbb1c783219084743951a594344f6cd0a
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-12 11:29:02 +0100

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get    "/tasks",          to: "tasks#index"
+  # Create
+  # get    "/tasks/new",      to: "tasks#new",  as: :new_restaurant
+  # post   "/tasks",          to: "tasks#create"
+  # # Read one - The `show` route needs to be *after* `new` route.
+  # get    "/tasks/:id",      to: "tasks#show", as: :restaurant
+  # # Update
+  # get    "/tasks/:id/edit", to: "tasks#edit", as: :edit_restaurant
+  # patch  "/tasks/:id",      to: "tasks#update"
+  # # Delete
+  # delete "/tasks/:id",      to: "tasks#destroy"
 end
